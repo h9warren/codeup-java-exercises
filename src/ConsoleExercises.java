0def04d698897ef3ba3951047b2023d0b431e6c5
Hunter Warren <hunter.warren@gmail.com>
2017-10-23 14:10:41 -0500

    getting familiar with scanner

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,32 @@
 import java.util.Scanner;
 
 public class ConsoleExercises {
-    public static void main(){
-        byte userInt;
+    public static void main(String[] args){
+//        int userString;
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter a number: ");
+//        userString = scan.nextInt();
+//
+//        System.out.println("You entered: --> \"" + userString + "\" <--");
+
+//        String userString;
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter 3 words: ");
+//        userString = scan.next();
+//        String userString2 = scan.next();
+//        String userString3 = scan.next();
+//
+//        System.out.println("You entered: --> \"" + userString + userString2 + userString3 + "\" <--");
+
+//        String sentence;
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Type a sentence: ");
+//        sentence = scan.nextLine();
+//
+//        System.out.println("You entered: --> \"" + sentence + "\" <--");
 
     }
 }
