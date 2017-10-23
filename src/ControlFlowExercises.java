029f7cd9578d3145a8b3ae16b4270d7482ced2bc
Hunter Warren <hunter.warren@gmail.com>
2017-10-23 17:00:50 -0500

    Counting backwards by 5's.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,14 +2,14 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int num = 2;
+        int num = 100;
 
         do {
 
             System.out.println(num);
-            num +=2;
+            num -= 5;
 
-        } while (num <= 100);
+        } while (num >= -10);
     }
 
 }
