9c69986d098cc0b93dc7324af8f585ee4968f085
Hunter Warren <hunter.warren@gmail.com>
2017-10-25 12:49:18 -0500

    concatenation

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String message = "We don't need no ";
+        System.out.print(message + "education\n" + message + "thought control");
+
+    }
+}
