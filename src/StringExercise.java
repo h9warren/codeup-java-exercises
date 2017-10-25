93253e36804f268a0f976bee9211fe1c1eb0a347
Hunter Warren <hunter.warren@gmail.com>
2017-10-25 13:07:55 -0500

    escaping character!

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -3,10 +3,12 @@ public class StringExercise {
 
 //        String message = "We don't need no ";
 //        System.out.print(message + "education\n" + message + "thought control");
-        String message = "\"this\"";
-        System.out.print("Check " + message + " out!, "+ message.substring(0,1) + message.substring(4,5) + " inside of "+ message.substring(0,1) + message.substring(4,5) + "!");
-
-
+//        String message = "\"this\"";
+//        System.out.print("Check " + message + " out!, "+ message.substring(0,1) + message.substring(4,5) + " inside of "+ message.substring(0,1) + message.substring(4,5) + "!");
+//        String message = "In windows, the main drive is usually C:\\";
+//        System.out.print(message);
+        String message = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.print(message);
 
     }
 }
