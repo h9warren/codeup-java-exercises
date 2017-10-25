64b7dff642a855f5c8f8ccee23794bda3068ad24
Hunter Warren <hunter.warren@gmail.com>
2017-10-25 12:55:48 -0500

    concatenation and substring

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,8 +1,12 @@
 public class StringExercise {
     public static void main(String[] args) {
 
-        String message = "We don't need no ";
-        System.out.print(message + "education\n" + message + "thought control");
+//        String message = "We don't need no ";
+//        System.out.print(message + "education\n" + message + "thought control");
+        String message = "\"this\"";
+        System.out.print("Check " + message + " out!, "+ message.substring(0,1) + message.substring(4,5) + " inside of "+ message.substring(0,1) + message.substring(4,5) + "!");
+
+
 
     }
 }
