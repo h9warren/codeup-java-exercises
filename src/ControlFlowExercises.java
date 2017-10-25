da2f8c53cc53015fb013f673888115f4b3210e40
Hunter Warren <hunter.warren@gmail.com>
2017-10-25 11:18:08 -0500

    table of exponent multiples

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,22 +1,51 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        for(int i = 1; i <= 100; i++) {
+        Scanner scan = new Scanner(System.in);
 
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+        String yesorno = "yes";
+
+        while((yesorno == "yes")) {
+
+            System.out.println("What number would you like to go up to? ");
+            int integer = scan.nextInt();
+
+            String table = "\nHere is your table";
+            System.out.println(table + "\n");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+
+            for(int i = 0; i <= integer; i++) {
+
+                System.out.printf("%-6s", (int)Math.pow(i,1));
+                System.out.print(" | ");
+                System.out.printf("%-7s", (int)Math.pow(i,2));
+                System.out.print(" | ");
+                System.out.printf("%-6s", (int)Math.pow(i,3));
+                System.out.printf("\n");
             }
+            System.out.println("\nWould you like to continue?");
+
+            String response = scan.next();
+
+            if (! response.equals("yes")) {
+                break;
+            }
+
         }
-
-
 
     }
 
 }
+
+
+
+
+
+
+
+
+
